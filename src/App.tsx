d4e7882338f0891fc7d0a94578a9760a3a91f047
yaroslavn <yaroslav.n@open-tag.com>
2026-02-04 19:54:55 +0200

    next-code: ryn build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from "./app/router"
-import { Login } from './components/Login/Login'
 
 function App() {
   
